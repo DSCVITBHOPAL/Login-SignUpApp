6ab420b1a4c9c3455aa422ed31554407d3e707f8
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2022-03-09 22:38:12 +0530

    Completed

--- app/src/main/java/com/food/loginsignupapp/authentications/GoogleAuthActivity.java
+++ app/src/main/java/com/food/loginsignupapp/authentications/GoogleAuthActivity.java
@@ -43,7 +43,7 @@ public class GoogleAuthActivity extends Login {
 
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken(getString(R.string.myDefault_web_client_id))
                 .requestEmail()
                 .build();
 
