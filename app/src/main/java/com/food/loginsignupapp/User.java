91cde20a803d830667dced37a86ea1251cfe85ce
Shankar Lohar <shankarloharmail@gmail.com>
2022-03-09 14:00:58 +0530

    User Class made serializable

--- app/src/main/java/com/food/loginsignupapp/User.java
+++ app/src/main/java/com/food/loginsignupapp/User.java
@@ -1,9 +1,8 @@
 package com.food.loginsignupapp;
 
-import com.google.firebase.database.IgnoreExtraProperties;
+import java.io.Serializable;
 
-@IgnoreExtraProperties
-public class User {
+public class User implements Serializable {
     String name;
     String age;
     String email;
@@ -15,6 +14,38 @@ public class User {
         this.name = name;
         this.age = age;
         this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 }
